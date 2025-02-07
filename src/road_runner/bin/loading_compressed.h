ef4261a9fb22570cca2e7bf83ec7946ed78081c7
ClementineCurel <78617457+Clementine951@users.noreply.github.com>
2025-02-07 15:54:25 +0100

    code cleaning

--- src/road_runner/bin/loading_compressed.h
+++
@@ -1,9 +0,0 @@
-#ifndef LOADING_H
-#define LOADING_H
-
-#include "graph_data_compressed.h"
-#include <string>
-
-graph_data load_graph_data(const std::string& file_path);
-
-#endif // LOADING_H
--- src/server/loadingCompressed.h
+++
@@ -1,9 +0,0 @@
-#ifndef LOADING_H
-#define LOADING_H
-
-#include "graphDataCompressed.h"
-#include <string>
-
-graph_data load_graph_data(const std::string& file_path);
-
-#endif // LOADING_H
